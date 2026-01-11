30bad4750229878a71eb32cb7a534fb1c4118fc4
DESKTOP-7A652JT\jadha_8ummw6x <pjahdav@hersheys.com>
2026-01-10 22:13:53 -0500

    AddAdded Teaser component new

---
+++ blocks/teaser/teaser.js
@@ -0,0 +1,17 @@
+export default function decorate(block) {
+  // We take the two columns from the table
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`teaser-${cols.length}-cols`);
+
+  // Loop through the columns to identify content
+  cols.forEach((col) => {
+    const pic = col.querySelector('picture');
+    if (pic) {
+      // It's the image column
+      col.classList.add('teaser-image');
+    } else {
+      // It's the text column
+      col.classList.add('teaser-content');
+    }
+  });
+}
